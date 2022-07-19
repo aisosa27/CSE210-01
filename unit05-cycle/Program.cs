3f0f18cdd97bf283dfb3c207ee8f0e39737be2d9
aisosa27 <91696727+aisosa27@users.noreply.github.com>
2022-07-18 18:37:14 -0600

    Add files via upload

---
+++ unit05-cycle/Program.cs
@@ -0,0 +1,46 @@
+﻿using Unit05.Game.Casting;
+using Unit05.Game.Directing;
+using Unit05.Game.Scripting;
+using Unit05.Game.Services;
+
+
+namespace Unit05
+{
+    /// <summary>
+    /// The program's entry point.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Starts the program using the given arguments.
+        /// </summary>
+        /// <param name="args">The given arguments.</param>
+        static void Main(string[] args)
+        {
+            // create the cast
+            Cast cast = new Cast();
+            // cast.AddActor("food", new Food());
+            Color colorG = new Color(0, 255, 0);
+            Color colorR = new Color(255, 0, 0);
+            cast.AddActor("snake", new Snake(200, colorG));
+            cast.AddActor("score", new Score());
+            cast.AddActor("snake", new Snake(605, colorR));
+            
+
+            // create the services
+            KeyboardService keyboardService = new KeyboardService();
+            VideoService videoService = new VideoService(false);
+           
+            // create the script
+            Script script = new Script();
+            script.AddAction("input", new ControlActorsAction(keyboardService));
+            script.AddAction("update", new MoveActorsAction());
+            script.AddAction("update", new HandleCollisionsAction());
+            script.AddAction("output", new DrawActorsAction(videoService));
+
+            // start the game
+            Director director = new Director(videoService);
+            director.StartGame(cast, script);
+        }
+    }
+}
