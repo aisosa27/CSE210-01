24af25822759b6b779cb7d653f2bf003740893ee
aisosa27 <91696727+aisosa27@users.noreply.github.com>
2022-07-19 00:49:56 -0600

    Update Program.cs

--- final/Program.cs
+++ final/Program.cs
@@ -27,7 +27,7 @@ public static class Program
                 }
             }
 
-            var monitorKeyPresses = MonitorKeyPresses();
+            var key = MonitorKeyPresses();
 
             do
             {
@@ -46,7 +46,7 @@ public static class Program
             }
 
             cts.Cancel();
-            await monitorKeyPresses;
+            await key;
         }
     }
 }
